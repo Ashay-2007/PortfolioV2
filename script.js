6f099eb9496d64f8532721cc080ab694878d2d58
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-01-22 23:46:34 +0530

    added the Projects section

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ $(document).ready(function() {
         $('.menu-btn i').toggleClass("active");
     });
 });
+
+const mixer = mixitup('.portfolio__container', {
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 400
+    }
+});
+
+// Link active portfolio
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
+
+function activePortfolio(){
+    if(linkPortfolio){
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
