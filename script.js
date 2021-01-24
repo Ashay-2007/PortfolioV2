f1438e10b80c7e5700b1e7de57112a9469ec9c9c
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-01-24 18:45:10 +0530

    modified navbar and added footer, scrollButton

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ $(document).ready(function() {
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // silde up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     // toggle menu/navbar script
@@ -13,6 +22,20 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+});
+
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Freelancer", "Competitive Coder"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Freelancer", "Competitive Coder"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
 });
 
 const mixer = mixitup('.portfolio__container', {
